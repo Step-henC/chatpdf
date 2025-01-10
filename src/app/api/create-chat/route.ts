cd6d1db4f844e187b4589c8a32e0dccb79517c5e
Step-henC <stevecunningham@outlook.com>
2025-01-10 14:33:19 -0600

    upload pdf to backend route for chats...added error styling

---
+++ src/app/api/create-chat/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+
+// need @tanstack/react-query to send file_key to backend when we upload to S3
+//local to server queries
+export async function POST(req: Request, res: Response){
+try {
+    const body = await req.json()
+    const {file_key, file_name} = body;
+    console.log(file_name)
+    return NextResponse.json({message: 'success'})
+
+} catch (e) {
+    console.log(e)
+    return NextResponse.json({status: 500, error: "internal server error"})
+}
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
+import {ClerkProvider} from '@clerk/nextjs'
 import './globals.css'
 import { Metadata } from 'next'
+import Provider from '@/components/Provider'
+import {Toaster} from 'react-hot-toast'
 
+//wrap data in provider so that every component has access to react-query server cache on subsequent api calls
 export const metadata: Metadata = {
   title: "Chat PDF"
 }
@@ -18,11 +15,14 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
+      <Provider>
       <html lang="en">
         <body>
           {children}
+          <Toaster />
         </body>
       </html>
+      </Provider>
     </ClerkProvider>
   )
 }
